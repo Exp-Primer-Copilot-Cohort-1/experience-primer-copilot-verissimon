e640fd5a8f97f61ced6612987c55927bb8b27653
Veríssimo Terceiro <terceiroverissimo@gmail.com>
2023-11-07 18:25:15 +0000

    copilot second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMember() {
+    var member = document.getElementById("member");
+    var skills = document.getElementById("skills");
+    var experience = document.getElementById("experience");
+    var contact = document.getElementById("contact");
+    member.style.display = "block";
+    skills.style.display = "none";
+    experience.style.display = "none";
+    contact.style.display = "none";
+}
